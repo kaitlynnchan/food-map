51d1d2b0db0c52b8d4ed6641e18fca1427665a32
kaitlynnchan <kaitlynnchan@outlook.com>
2024-05-12 15:48:12 -0700

    Convert from Kotlin to Java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.play.services.maps)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/foodmap/ui/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.foodmap.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import org.jetbrains.annotations.Nullable;
+
+public final class LoginActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
