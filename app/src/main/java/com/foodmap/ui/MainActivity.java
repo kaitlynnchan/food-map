2a75d829398ee71fb98d50f2e7c3ee838840222b
kaitlynnchan <kaitlynnchan@outlook.com>
2024-05-21 18:57:07 -0700

    Logout working
    - on success logs user out and starts login
      activity

--- app/src/main/java/com/foodmap/ui/MainActivity.java
+++ app/src/main/java/com/foodmap/ui/MainActivity.java
@@ -1,28 +1,19 @@
 package com.foodmap.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.auth0.android.Auth0;
-import com.auth0.android.authentication.AuthenticationException;
-import com.auth0.android.callback.Callback;
-import com.auth0.android.provider.WebAuthProvider;
 import com.foodmap.databinding.ActivityMainBinding;
 
 import com.foodmap.R;
 import com.foodmap.ui.main.ListFragment;
 import com.foodmap.ui.main.MapsFragment;
 import com.foodmap.ui.main.ProfileFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private Auth0 account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // connect bottom navigation
         MapsFragment mapsFragment = new MapsFragment();
         ListFragment listFragment = new ListFragment();
         ProfileFragment profileFragment = new ProfileFragment();
 
-        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
-        navView.setOnItemSelectedListener(menuItem -> {
+        binding.bottomNavigationView.setOnItemSelectedListener(menuItem -> {
             switch (menuItem.getItemId()){
                 case (R.id.location):
                     getSupportFragmentManager()
@@ -59,27 +50,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         });
-
-//        account = new Auth0(this);
-//        Button logoutBtn = findViewById(R.id.signoutBtn);
-//        logoutBtn.setOnClickListener(v -> {
-//            WebAuthProvider
-//                    .logout(account)
-//                    .withScheme(getString(R.string.com_auth0_scheme))
-//                    .start(this, new Callback<Void, AuthenticationException>() {
-//
-//                        @Override
-//                        public void onSuccess(Void result) {
-//                            // close main activity
-//                            MainActivity.this.finish();
-//                        }
-//
-//                        @Override
-//                        public void onFailure(@NonNull AuthenticationException e) {
-//                            System.out.println("Exception error: " + e.getMessage());
-//                        }
-//                    });
-//        });
     }
 
 }
--- app/src/main/java/com/foodmap/ui/main/ProfileFragment.java
+++ app/src/main/java/com/foodmap/ui/main/ProfileFragment.java
@@ -1,14 +1,22 @@
 package com.foodmap.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.callback.Callback;
+import com.auth0.android.provider.WebAuthProvider;
 import com.foodmap.R;
+import com.foodmap.ui.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,15 +25,14 @@ import com.foodmap.R;
  */
 public class ProfileFragment extends Fragment {
 
+    private Auth0 account;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
     }
 
     @Override
@@ -39,6 +46,35 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        account = new Auth0(
+                getString(R.string.com_auth0_client_id),
+                getString(R.string.com_auth0_domain)
+        );
+        Button logoutBtn = view.findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(v -> logout());
+        return view;
+    }
+
+    private void logout(){
+        WebAuthProvider
+            .logout(account)
+            .withScheme(getString(R.string.com_auth0_scheme))
+            .start(getView().getContext(), new Callback<Void, AuthenticationException>() {
+
+                @Override
+                public void onSuccess(Void result) {
+                    // close main activity
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+
+                @Override
+                public void onFailure(@NonNull AuthenticationException e) {
+                    System.out.println("Exception error: " + e.getMessage());
+                }
+            });
     }
 }
